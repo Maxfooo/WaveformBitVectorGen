a4e36dd904a0c91fd6146e342b0045476bc09b75
Max <maxfooo@gmail.com>
2016-05-06 16:20:42 -0700

    Update GenerateSinusoid.py

--- GenerateSinusoid.py
+++ GenerateSinusoid.py
@@ -14,12 +14,13 @@ class GenerateSinusoid(object):
     '''
     b111001100000,1,1,1,0,0,1,1,0,0,0,0,0
     '''
-    def __init__(self, busName, fileBufferSize, bitResolution, upLimit=None, loLimit=None):
+    def __init__(self, busName, fileBufferSize, bitResolution, upLimit=None, loLimit=None, p2pShrink=None):
         self.busName = busName
         self.fileBufferSize = fileBufferSize
         self.bitResolution = bitResolution
         self.upLimit = upLimit
         self.loLimit = loLimit
+        self.p2pShrink = p2pShrink
         self.fileIO = FileIO()
 
     def run(self):
@@ -39,26 +40,33 @@ class GenerateSinusoid(object):
         self.fileHeader = header.format(self.busName)
 
     def genSinusoid(self):
-        sizeByte_2 = (2**self.bitResolution)/2
+        sizeByte_2 = (2**self.bitResolution - 1)/2
         sinStepSize = (2*m.pi) / self.fileBufferSize
         self.byteValues = []
 
         if (self.upLimit == None and self.loLimit == None):
-            for j in range(self.fileBufferSize):
-                b = format((round(sizeByte_2+((sizeByte_2)*m.sin(j*sinStepSize))))-1,'0{}b'.format(self.bitResolution))
-                b = 'b'+ b
-                self.byteValues.append(b)
+            if self.p2pShrink == None:
+                for j in range(self.fileBufferSize):
+                    b = format((round(sizeByte_2+(sizeByte_2*m.sin(j*sinStepSize)))),'0{}b'.format(self.bitResolution))
+                    b = 'b'+ b
+                    self.byteValues.append(b)
+            else:
+                sineP2P = (sizeByte_2 - self.p2pShrink)/2
+                for j in range(self.fileBufferSize):
+                    b = format((round(sizeByte_2+(sineP2P*m.sin(j*sinStepSize)))),'0{}b'.format(self.bitResolution))
+                    b = 'b'+ b
+                    self.byteValues.append(b)
         else:
             sineUpLimFactor = self.upLimit/2
             sineLoLimFactor = sizeByte_2 - self.loLimit
             for j in range(self.fileBufferSize):
                 sineVal = m.sin(j*sinStepSize)
                 if sineVal >= 0:
-                    b = format((round(sizeByte_2+(sineUpLimFactor*sineVal)))-1,'0{}b'.format(self.bitResolution))
+                    b = format((round(sizeByte_2+(sineUpLimFactor*sineVal))),'0{}b'.format(self.bitResolution))
                     b = 'b'+ b
                     self.byteValues.append(b)
                 else:
-                    b = format((round(sizeByte_2+(sineLoLimFactor*sineVal)))-1,'0{}b'.format(self.bitResolution))
+                    b = format((round(sizeByte_2+(sineLoLimFactor*sineVal))),'0{}b'.format(self.bitResolution))
                     b = 'b'+ b
                     self.byteValues.append(b)
 
@@ -78,5 +86,4 @@ class GenerateSinusoid(object):
         for i in self.byteValues:
             waveFile.write(i + self.genCSVbyte(i) + '\n')
         waveFile.close()
-
 
